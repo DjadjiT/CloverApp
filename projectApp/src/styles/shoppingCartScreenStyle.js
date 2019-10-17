9f76b93c750598d013de51a6eaf1bb21314d6d31
Kaalawolf <dabinmegane@gmail.com>
2019-10-17 18:06:36 +0200

    add shopping cart screen style

---
+++ projectApp/src/styles/shoppingCartScreenStyle.js
@@ -0,0 +1,32 @@
+import {StyleSheet, Platform, Dimensions} from 'react-native';
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize,
+} from 'react-native-responsive-dimensions';
+
+const styles = StyleSheet.create({
+  mainView: {
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  view: {
+    width: responsiveWidth(100),
+    alignItems: 'center',
+    borderStyle: 'solid',
+    borderBottomWidth: 1,
+  },
+  text: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: responsiveFontSize(2.5)
+  },
+  userText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: responsiveFontSize(3)
+  }
+});
+
+module.exports = styles;
