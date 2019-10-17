0ba5dc9cba924f496b35ccf1ffe40157db09bd8c
Lukas Brasseleur <lukasbrasseleur@MacBook-Pro-de-Lukas.local>
2019-10-17 10:21:04 +0200

    Add api Call and update customize Screen

--- projectApp/src/actions/apiCall.js
+++ projectApp/src/actions/apiCall.js
@@ -1,6 +1,48 @@
 import {Platform, Linking} from 'react-native';
-import NetInfo from '@react-native-community/netinfo';
+import NetInfo from 'react-native-netinfo';
 
-import config from '../config/config';
+export async function goToCommunity() {
+  // await callfunction then
+  Actions.replace('communityScreen');
+}
 
-export async function goToCommunity() {}
+export async function goToCommunityDetails() {
+  // await callfunction then
+  Actions.replace('communityDetailsScreen');
+}
+
+export async function goToCustomizer() {
+  // await callfunction then
+  Actions.replace('customizerScreen');
+}
+
+export async function goToUserAccount() {
+  // await callfunction then
+  Actions.replace('mainScreen');
+}
+
+export async function makeGetCall(url) {
+  return new Promise(function(resolve, reject) {
+    try {
+      let response = fetch(url, {
+        method: 'GET',
+      })
+        .then(response => {
+          if (!response.ok) {
+            reject(response.status);
+          } else {
+            resolve(response.json());
+          }
+        })
+        .catch(error => reject(error));
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+export async function share(watchList) {
+  return new Promise(function(resolve, reject) {
+    resolve(true);
+  });
+}
--- projectApp/src/container/customizerScreen.js
+++ projectApp/src/container/customizerScreen.js
@@ -8,143 +8,225 @@
 
 import React, {Component, setGlobal} from 'reactn';
 import {
-    Platform,
-    Text,
-    View,
-    Image,
-    TouchableOpacity,
-    Dimensions,
-    ScrollView,
+  Platform,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  ScrollView,
 } from 'react-native';
 
 import {Actions} from 'react-native-router-flux';
 import Carousel from 'react-native-snap-carousel';
 import {
-    responsiveHeight,
-    responsiveWidth,
-    responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize,
 } from 'react-native-responsive-dimensions';
+
+import * as ApiCall from '../actions/apiCall';
 
 import styles from '../styles/mainScreenStyle';
 
 type Props = {};
 
 const componentsArray = [
-    {title: 'boitier', image: require('../../assets/watch_case.png')},
-    {title: 'cadran', image: require('../../assets/watch_clock_dial.png')},
-    {title: 'aiguilles', image: require('../../assets/watch_hand.png')},
-    {title: 'bracelet', image: require('../../assets/watchband.png')},
+  {title: 'boitier', image: require('../../assets/watch_case.png')},
+  {title: 'cadran', image: require('../../assets/watch_clock_dial.png')},
+  {title: 'aiguilles', image: require('../../assets/watch_hand.png')},
+  {title: 'bracelet', image: require('../../assets/watchband.png')},
 ];
 
 export default class CustomizerScreen extends Component<Props> {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            currentComponent: null,
-            itemListView: null,
-        };
-    }
+    this.state = {
+      currentComponent: null,
+      itemListView: null,
+      watchList: {},
+    };
+  }
 
-    addList(currentItem) {
-        this.setState({
-            itemListView: (
-                <ScrollView
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
-                    style={{
-                        flex: 1,
-                    }}>
-                    {componentsArray.map((item, index) => {
-                        return (
-                            <TouchableOpacity
-                                key={index}
-                                style={{
-                                    flex: 1,
-                                    borderWidth: 1,
-                                    borderColor: 'lightgrey',
-                                    flexDirection: 'column',
-                                    paddingHorizontal: responsiveWidth(
-                                        componentsArray.length >= 5 ? 5 : 12,
-                                    ),
-                                }}>
-                                <Image
-                                    source={require('../../assets/burger.png')}
-                                    resizeMode="contain"
-                                    style={{height: 30, width: 30, alignSelf: 'center'}}
-                                />
-                                <Text style={{flex: 1, alignSelf: 'center'}}>{item.title}</Text>
-                            </TouchableOpacity>
-                        );
-                    })}
-                </ScrollView>
-            ),
-            currentComponent: currentItem,
-        });
-    }
+  addList(currentItem) {
+    this.setState({
+      itemListView: (
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{
+            flex: 1,
+          }}>
+          {componentsArray.map((item, index) => {
+            return (
+              <TouchableOpacity
+                key={index}
+                style={{
+                  flex: 1,
+                  borderWidth: 1,
+                  borderColor: 'lightgrey',
+                  flexDirection: 'column',
+                  paddingHorizontal: responsiveWidth(
+                    componentsArray.length >= 5 ? 5 : 12,
+                  ),
+                }}>
+                <Image
+                  source={require('../../assets/burger.png')}
+                  resizeMode="contain"
+                  style={{height: 30, width: 30, alignSelf: 'center'}}
+                />
+                <Text style={{flex: 1, alignSelf: 'center'}}>{item.title}</Text>
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      ),
+      currentComponent: currentItem,
+    });
+  }
 
-    render() {
-        let {currentComponent, itemListView} = this.state;
+  render() {
+    let {currentComponent, itemListView, watchList} = this.state;
 
-        return (
-            <View style={styles.mainView}>
-                <View>
-                    <Image
-                        source={require('../../assets/watches/bracelet_cuir.png')}
-                        resizeMode="contain"
-                        style={{height: 650, width: 650, alignSelf: 'center', position: 'absolute', top: 15}}
-                    />
-                    <Image
-                        source={require('../../assets/watches/cadre_0.png')}
-                        resizeMode="contain"
-                        style={{height: 100, width: 100, alignSelf: 'center', position: 'absolute', top: 250}}
-                    />
-                    <Image
-                        source={require('../../assets/watches/boitier_A.png')}
-                        resizeMode="contain"
-                        style={{height: 150, width: 150, alignSelf: 'center', position: 'absolute', top: 230}}
-                    />
-                </View>
-                <View
-                    style={{
-                        flexDirection: 'column',
-                        position: 'absolute',
-                        width: '100%',
-                        bottom: 0,
-                        alignSelf: 'stretch',
-                    }}>
-                    {itemListView}
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            flex: 1,
-                            justifyContent: 'center',
-                        }}>
-                        {componentsArray.map((item, index) => {
-                            return (
-                                <TouchableOpacity
-                                    key={item.title}
-                                    style={{
-                                        flex: 1,
-                                        borderWidth: 1,
-                                        borderColor: 'lightgrey',
-                                        flexDirection: 'column',
-                                    }}
-                                    onPress={() => this.addList(item)}>
-                                    <Image
-                                        source={item.image}
-                                        resizeMode="contain"
-                                        style={{height: 30, width: 30, alignSelf: 'center'}}
-                                    />
-                                    <Text style={{flex: 1, alignSelf: 'center'}}>
-                                        {item.title}
-                                    </Text>
-                                </TouchableOpacity>
-                            );
-                        })}
-                    </View>
-                </View>
-            </View>
-        );
-    }
+    return (
+      <View style={styles.mainView}>
+        <View
+          key="actions"
+          style={{
+            position: 'absolute',
+            top: 150,
+            right: 0,
+            flexDirection: 'column',
+            zIndex: 1,
+          }}>
+          <TouchableOpacity
+            style={{
+              borderColor: 'lightgrey',
+              borderWidth: 1,
+            }}
+            onPress={() => {
+              ApiCall.share(watchList)
+                .then(() => {
+                  Actions.replace('MainScreen');
+                })
+                .catch(error => {
+                  console.log(error);
+                  Actions.replace('MainScreen');
+                });
+            }}>
+            <Image
+              source={require('../../assets/share.png')}
+              resizeMode="contain"
+              style={{
+                height: 40,
+                width: 40,
+                alignSelf: 'center',
+                margin: responsiveWidth(2),
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{borderColor: 'lightgrey', borderWidth: 1}}
+            onPress={() => {
+              ApiCall.share(watchList)
+                .then(() => {
+                  Actions.replace('MainScreen');
+                })
+                .catch(error => {
+                  console.log(error);
+                  Actions.replace('MainScreen');
+                });
+            }}>
+            <Image
+              source={require('../../assets/add_to_cart.png')}
+              resizeMode="contain"
+              style={{
+                height: 40,
+                width: 40,
+                alignSelf: 'center',
+                margin: responsiveWidth(2),
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Image
+            source={require('../../assets/watches/bracelet_cuir.png')}
+            resizeMode="contain"
+            style={{
+              height: 650,
+              width: 650,
+              alignSelf: 'center',
+              position: 'absolute',
+              top: 15,
+            }}
+          />
+          <Image
+            source={require('../../assets/watches/cadre_0.png')}
+            resizeMode="contain"
+            style={{
+              height: 100,
+              width: 100,
+              alignSelf: 'center',
+              position: 'absolute',
+              top: 250,
+            }}
+          />
+          <Image
+            source={require('../../assets/watches/boitier_A.png')}
+            resizeMode="contain"
+            style={{
+              height: 150,
+              width: 150,
+              alignSelf: 'center',
+              position: 'absolute',
+              top: 230,
+            }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: 'column',
+            position: 'absolute',
+            width: '100%',
+            bottom: 0,
+            alignSelf: 'stretch',
+          }}>
+          {itemListView}
+          <View
+            style={{
+              flexDirection: 'row',
+              flex: 1,
+              justifyContent: 'center',
+            }}>
+            {componentsArray.map((item, index) => {
+              return (
+                <TouchableOpacity
+                  key={item.title}
+                  style={{
+                    flex: 1,
+                    borderWidth: 1,
+                    borderColor: 'lightgrey',
+                    flexDirection: 'column',
+                    backgroundColor:
+                      item === currentComponent ? 'white' : '#F6F6F6',
+                  }}
+                  onPress={() => this.addList(item)}>
+                  <Image
+                    source={item.image}
+                    resizeMode="contain"
+                    style={{height: 30, width: 30, alignSelf: 'center'}}
+                  />
+                  <Text style={{flex: 1, alignSelf: 'center'}}>
+                    {item.title}
+                  </Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
