2c15b76a0514c1598568b59db292772c5a8d147b
Lukas Brasseleur <lukasbrasseleur@MacBook-Pro-de-Lukas.local>
2019-10-18 11:39:19 +0200

    Add opacity on cards

--- projectApp/src/component/communityBlock.js
+++ projectApp/src/component/communityBlock.js
@@ -47,108 +47,115 @@ class CommunityBlock extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {favorite : this.props.favorite};
+    this.state = {favorite: this.props.favorite};
   }
 
   render() {
     return (
-        <View style={{marginVertical: responsiveHeight(3),}}>
-            <TouchableOpacity
-                onPress={this.props.onPress}
+      <View style={{marginVertical: responsiveHeight(3)}}>
+        <TouchableOpacity
+          onPress={this.props.onPress}
+          style={{
+            height: responsiveHeight(30),
+            width: responsiveWidth(80),
+            backgroundColor: 'white',
+            flexDirection: 'row',
+            alignItems: 'center',
+            elevation: 10,
+            shadowColor: '#AAA',
+            shadowOpacity: 1,
+            shadowRadius: 10,
+          }}>
+          <Image
+            source={this.props.source}
+            resizeMode="contain"
+            style={{flex: 1, height: responsiveHeight(30)}}
+          />
+          <View style={{flex: 1.5}}>
+            <View style={{flex: 2, justifyContent: 'flex-end'}}>
+              <Text
+                style={{fontWeight: 'bold', marginVertical: 5}}
+                numberOfLines={1}>
+                {this.props.title}
+              </Text>
+              <Text
+                numberOfLines={4}
+                ellipsizeMode="tail"
                 style={{
-                    height: responsiveHeight(30),
-                    width: responsiveWidth(80),
-                    backgroundColor: 'white',
-                    flexDirection: 'row',
-                    alignItems: 'center',
+                  width: responsiveWidth(45),
+                  marginHorizontal: responsiveWidth(2),
                 }}>
-                <Image
-                    source={this.props.source}
-                    resizeMode="contain"
-                    style={{flex: 1, height: responsiveHeight(30)}}
-                />
-                <View style={{flex: 1.5}}>
-                    <View style={{flex: 2, justifyContent: 'flex-end'}}>
-                        <Text
-                            style={{fontWeight: 'bold', marginVertical: 5}}
-                            numberOfLines={1}>
-                            {this.props.title}
-                        </Text>
-                        <Text
-                            numberOfLines={4}
-                            ellipsizeMode="tail"
-                            style={{
-                                width: responsiveWidth(45),
-                                marginHorizontal: responsiveWidth(2),
-                            }}>
-                            {this.props.text}
-                        </Text>
-                    </View>
-                    <View style={{flex: 1, justifyContent: 'flex-end'}}>
-                        <Text
-                            style={{
-                                color: 'lightgrey',
-                                marginBottom: responsiveHeight(2),
-                                alignSelf: 'flex-end',
-                                marginRight: responsiveWidth(5),
-                            }}>
-                            Voir plus >>
-                        </Text>
-                    </View>
-                </View>
-            </TouchableOpacity>
-            <View style = {{
-                backgroundColor: '#FFF',
-                flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'space-between'
-            }}>
-                <TouchableOpacity style = {{margin: 10}}>
-                    <Image
-                        source={require("../../assets/baseline_star_border_black_18dp.png")}
-                        resizeMode="contain"
-                    />
-                </TouchableOpacity>
-
-                {this.displayFavorite()}
-
-                <TouchableOpacity style = {{margin: 10,
-                }}>
-                    <Image
-                        source={require("../../assets/baseline_comment_black_18dp.png")}
-                        resizeMode="contain"
-                    />
-                </TouchableOpacity>
+                {this.props.text}
+              </Text>
             </View>
+            <View style={{flex: 1, justifyContent: 'flex-end'}}>
+              <Text
+                style={{
+                  color: 'lightgrey',
+                  marginBottom: responsiveHeight(2),
+                  alignSelf: 'flex-end',
+                  marginRight: responsiveWidth(5),
+                }}>
+                Voir plus >>
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+        <View
+          style={{
+            backgroundColor: '#FFF',
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <TouchableOpacity style={{margin: 10}}>
+            <Image
+              source={require('../../assets/baseline_star_border_black_18dp.png')}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+
+          {this.displayFavorite()}
+
+          <TouchableOpacity style={{margin: 10}}>
+            <Image
+              source={require('../../assets/baseline_comment_black_18dp.png')}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
         </View>
+      </View>
     );
   }
-    displayFavorite(){
-        if(this.props.favorite){
-            return (<TouchableOpacity style = {{margin: 10}} onPress = {() => {
-                this.setState({favorite: !this.state.favorite});
-            }
-            }>
-                <Image
-                    source={require("../../assets/favorite-24px.png")}
-                    resizeMode="contain"
-                />
-            </TouchableOpacity>);
-        }
-        else {
-            return (<TouchableOpacity style = {{margin: 10}}
-                                      onPress ={() => {
-                                          this.setState({favorite: !this.state.favorite});
-                                      }
-                                      }
-            >
-                <Image
-                    source={require("../../assets/baseline_favorite_border_black_18dp.png")}
-                    resizeMode="contain"
-                />
-            </TouchableOpacity>);
-        }
+  displayFavorite() {
+    if (this.props.favorite) {
+      return (
+        <TouchableOpacity
+          style={{margin: 10}}
+          onPress={() => {
+            this.setState({favorite: !this.state.favorite});
+          }}>
+          <Image
+            source={require('../../assets/favorite-24px.png')}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      );
+    } else {
+      return (
+        <TouchableOpacity
+          style={{margin: 10}}
+          onPress={() => {
+            this.setState({favorite: !this.state.favorite});
+          }}>
+          <Image
+            source={require('../../assets/baseline_favorite_border_black_18dp.png')}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      );
     }
+  }
 }
 
 export default CommunityBlock;
